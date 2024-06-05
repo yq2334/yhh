07921c1ab26cf57e9fe8584d11a903ac9910d6c6
yq2334 <1223051857@qq.com>
2024-06-05 16:57:16 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,32 @@
+// 应用全局配置
+export default {
+	// 应用信息
+	appInfo: {
+		// 应用名称
+		name: "翼宏惠",
+		// 应用版本
+		version: "1.1.0",
+		// 应用logo
+		logo: "/static/images/logo.png",
+		// 官方网站
+		site_url: "http://www.izhaorui.cn",
+		// 政策协议
+		agreements: [{
+				title: "钱宝科技线下收单业务服务合作协议",
+				url: "https://jlsh.globebill.com/safe_shoudan.html"
+			},
+			{
+				title: "商户移动应用端商户注册协议",
+				url: "https://jlsh.globebill.com/safe_zhuce.html"
+			},
+			{
+				title: "隐私保护政策",
+				url: "https://jlsh.globebill.com/safe_yinsi.html"
+			},
+		]
+	},
+	api: {
+		fid: 'yhhsh',
+		fkey: 'ygb5kd392HuX507yt84dx2'
+	}
+}
