2ecff5a4d5c65b342ab6c37c2724d314f5964037
yq2334 <1223051857@qq.com>
2024-06-05 16:52:09 +0800

    Add files via upload

---
+++ store/getters.js
@@ -0,0 +1,12 @@
+const getters = {
+  token: state => state.user.token,
+  avatar: state => state.user.avatar,
+  name: state => state.user.name,
+  roles: state => state.user.roles,
+  permissions: state => state.user.permissions,
+  userId: state => state.user.userInfo.userId,
+  userInfo: state => state.user.userInfo,
+  sysConfig: state => state.user.sysConfig,
+  snConfig: state => state.user.snConfig,
+}
+export default getters
